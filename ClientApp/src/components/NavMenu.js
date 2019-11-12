8e595929385776bea41c80ed297653fd18af9a03
Karoline Ådnanes <karoline.adnanes@gmail.com>
2019-11-12 16:01:02 +0100

    lagt til logo

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import './NavMenu.css';
+import Logo from './VyLogo.png';
 
 export class NavMenu extends Component {
     static displayName = NavMenu.name;
@@ -26,7 +27,7 @@ export class NavMenu extends Component {
             <header>
                 <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
                     <Container>
-                        <NavbarBrand tag={Link} to="/"  className="text-white"> Kundeservice </NavbarBrand>
+                        <NavbarBrand tag={Link} to="/" className="text-white"> <img className='logo' src={Logo} alt='VyLogo'/>Kundeservice </NavbarBrand>
                         <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
                         <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
                             <ul className="navbar-nav flex-grow">
